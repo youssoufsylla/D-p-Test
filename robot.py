271d15e83ac4fdc50bca59a1b4286038f930bd84
Youssoufsylla <yousylla1234@gmail.com>
2025-04-29 15:00:46 +0000

    erreur

--- robot.py
+++ robot.py
@@ -1,4 +1,14 @@
-print ("salut")
-print("salut moussa")
-print("youssouf")
-print ("je veux manger")
+
+import sqlite3
+
+def get_user_data(username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    
+    # ⚠️ Vulnérable à une injection SQL
+    query = f"SELECT * FROM users WHERE username = '{username}'"
+    cursor.execute(query)
+    
+    result = cursor.fetchall()
+    conn.close()
+    return result
